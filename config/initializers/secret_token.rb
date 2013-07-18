10e388e7d19f8b779cd6cf5d274b0a7ac5b3c35c
Enzo Cappa <ecappa@squeeze-test.squeeze-test>
2013-07-18 17:08:24 -0300

    Cosmetic changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,7 +7,7 @@ require File.join(Rails.root,'lib','openshift_secret_generator.rb')
 # no regular words or you'll be exposed to dictionary attacks.
 
 # Set token based on intialize_secret function (defined in initializers/secret_generator.rb)
-EcoFamily::Application.config.secret_token = initialize_secret(
+EcoFamily::Application.config.secret_key_base = initialize_secret(
   :token,
   '335a4e365ef2daeea969640d74e18f0e3cd9fae1abd8f4125691a880774ea6d456a29c0831aa6921bf86a710fe555e916f0673f5657619ec9df22e0409bec345'
 )
