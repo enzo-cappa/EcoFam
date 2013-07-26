e238eedddd179d27d763303da58b906a589e8708
Enzo Cappa <zcappa@gmail.com>
2013-07-26 19:47:32 -0300

    Table name.field for order

--- lib/monthly/monthly.rb
+++ lib/monthly/monthly.rb
@@ -7,7 +7,8 @@ module Monthly
 
     module ClassMethods
       def acts_as_monthly(options = {})
-        default_scope -> { order("#{self.name.underscore}_date ASC, created_at ASC") }
+        table_name = self.table_name
+        default_scope -> { order("#{table_name}.#{self.name.underscore}_date ASC, #{table_name}.created_at ASC") }
         scope :at_month, lambda { |initial_date|
           where("#{self.name.underscore}_date >= ? and #{self.name.underscore}_date < ?", initial_date, initial_date.next_month.beginning_of_month)
         }
