0583ebc609b7bee34485dfa87d869c45f67eec3b
Enzo Cappa <ecappa@squeeze-test.squeeze-test>
2014-02-06 17:04:20 -0300

    Price variation calculation

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,17 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true, uniqueness: true
 
   default_scope {order(:name)}
+
+  def annual_price_rise
+    prices = price_lines.where{(date > Time.now - 12.months)}.order('DATE ASC')
+    first_price = prices.first
+    last_price = prices.last
+    unless first_price.nil? 
+      result = (last_price.price - first_price.price)* 100 / first_price.price
+    else
+      result = 0.0
+    end
+    result = result.to_i unless result.nan?
+    return result
+  end
 end
